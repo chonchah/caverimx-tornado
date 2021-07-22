98b075cb91d7b59e317cb210d38dd63957d1b522
Alexis Betancourt <chonchah@gmail.com>
2021-07-22 14:31:40 -0500

    main route serve static files from ./src/recursos/public

--- src/app/router.py
+++ src/app/router.py
@@ -1,12 +1,14 @@
 import handlers.websocket
 import tornado.web
-import handlers.websocket, handlers.home
-settings={
-    'static_path':'/recursos/public',
+import handlers.websocket
+import handlers.home
+settings = {
+    'static_path': '/recursos/public',
 }
 
-routes=[
-    (r"/websocket",handlers.websocket.WebSocket),
-    (r"/",handlers.home.MainHandler),
-    (r"/(.*)", tornado.web.StaticFileHandler, dict(path=settings['static_path']) ),
+routes = [
+    (r"/websocket", handlers.websocket.WebSocket),
+    (r"/api", handlers.home.MainHandler),
+    (r"/(.*)", tornado.web.StaticFileHandler,
+     dict(path=settings['static_path'])),
 ]
